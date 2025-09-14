563709e37fcbc80e2e6a748e80940d3ecab9379c
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-14 04:12:10 +0000

    white colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={cn('font-body antialiased dark')}>
+      <body className={cn('font-body antialiased')}>
         {children}
         <Toaster />
       </body>
