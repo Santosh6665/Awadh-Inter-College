06c0e1f593cd7ef94e58bc7ecd343cfe6d7e55d6
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-14 04:11:23 +0000

    morden look

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={cn('font-body antialiased')}>
+      <body className={cn('font-body antialiased dark')}>
         {children}
         <Toaster />
       </body>
