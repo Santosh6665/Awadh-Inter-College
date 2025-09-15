4f2cdbf0e5dc6dfe1fc5ff5e998115388952276c
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-15 07:40:54 +0000

    in side admin page all feature make working and connect to database

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,5 +1,5 @@
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './card';
 import { Users, UserPlus, FileText, BarChart2, Library, Bus } from 'lucide-react';
 import Link from 'next/link';
 
