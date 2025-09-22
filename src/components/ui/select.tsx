45e195007f673c4ce877f42fa7e09709efbafde7
Santosh Chaudhary <santoshchaudhary6387037649@gmail.com>
2025-09-22 12:30:03 +0000

    select class drop down over flow add student card fix issues

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -88,8 +88,9 @@ const SelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
+          "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "min-w-[var(--radix-select-trigger-width)]"
         )}
       >
         {children}
