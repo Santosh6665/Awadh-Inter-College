709eebbc1b31099563023652b48495e828182273
santoshchaudhary6387037649 <santoshchaudhary6387037649@gmail.com>
2025-09-26 08:31:19 +0000

    Find Host Bug 4

--- src/app/teacher/attendance/attendance-management.tsx
+++ src/app/teacher/attendance/attendance-management.tsx
@@ -59,7 +59,7 @@ export function AttendanceManagement({ students, teacher }: { students: Student[
   const checkDateStatus = useCallback(async () => {
     setLoading(true);
     const holidayStatus = await isHoliday(formattedDate);
-    console.log("CONSOLE:",isHoliday, isHoliday("2025-09-22"));
+    console.log("CONSOLE:",isHoliday, await isHoliday("2025-09-22"));
     setIsDateHoliday(holidayStatus.isHoliday);
     setHolidayName(holidayStatus.name || 'Holiday');
 
